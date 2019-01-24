034c6d97b9b975693e068f70e0355d8c1b698060
Kaleb James Gaar <kgaar@cse.unl.edu>
2019-01-24 16:48:02 -0600

    current work

---
+++ javascript/controller.js
@@ -0,0 +1,33 @@
+var current = "wave";
+var styleArray = ["matrix", "wave"];
+
+$(document).ready(function(){
+
+	var changeStyle = function(name){
+		var $sheet = $("<link class='signStyle' rel='stylesheet' href='../stylesheets/"+name+".css'>");
+		$(".signStyle").remove();
+		$("head").append($sheet);
+	}
+	
+	$(document).keypress(function(event){ 
+		var key = (event.keyCode ? event.keyCode : event.which); 
+		if( key == "32"){
+			cycleStyle();
+		}  
+	});
+	
+	var cycleStyle = function(){
+		var index = styleArray.findIndex(function(e){
+			return e == current;
+		});
+		var newIndex = (index + 1) % styleArray.length;
+		console.log(index);
+		current = styleArray[newIndex];
+		changeStyle(current);
+		
+		
+	};
+	
+	cycleStyle();
+
+});
